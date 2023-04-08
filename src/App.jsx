256255a1f3635a97c7777c6864b7f432e272b5d6
Md Arefine Ahamed Joy <mdarefine05@gmail.com>
2023-04-08 12:14:08 +0600

    fast setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <button className='bg-green-500'>hello</button>
+    </div>
+  )
+}
+
+export default App
